2e3993ea3e7f91b16c5fbc8e3f5a135e0b5e5e01
pivnati <natanmuleta911@gmail.com>
2025-04-14 15:03:47 -0700

    madd the dynamic route in the in the index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express'
-const app = express()
+const app = express() //  This line creates an instance of an Express application. 
 const PORT = 3000
 
 // define simple route
 app.get('/' , (req,res)=>{
-     res.send('Hello , this is Express')
+     res.send('Hello , this is piv ')
+})
+  // dynamic route of express 
+app.get('/user/:username', (req,res)=>{
+  const username = req.params.username;
+  res.send(`welcome ${username}`)
 })
 
 app.listen(PORT,()=>{
