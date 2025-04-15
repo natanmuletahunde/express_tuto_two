08456f97f38d6cb96769c606daa68bace09d6e44
pivnati <natanmuleta911@gmail.com>
2025-04-15 14:51:23 -0700

    create the app.use() register middleware

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 import express from 'express'
-import router from './route.js'
-const app = express() //  This line creates an instance of an Express application. 
+const app = express()
 const PORT = 3000
 
-// define simple route
-app.get('/' , (req,res)=>{
-     res.send('Hello , this is piv ')
-})
-app.use('/user',router)
-
-app.post('/users',express.json(), (req,res)=>{
-    const {name,email} = req.body
-    res.json({
-        message:`my my name is ${name} is  and my email is ${email}`
+app.use((req,res,next)=>{   // register middleware consist of req,res and next()  inside it 
+    console.log('start')
+    res.on('finish', ()=>{
+        console.log('end')
     })
+    next()
 })
-
-app.put('/users/:id',express.json(),(req,res)=>{
-    const userId = req.params.id
-    const {name , email} = req.body
-   res.json({
-    message:`the updated name is ${name} and email is ${email}`
-   })
+app.get('/', (req, res) => {
+      console.log('middle') 
+    res.send('Hello, this is piv')
 })
-app.listen(PORT,()=>{
+// Start the server
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
  
