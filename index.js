eb8535f7c6cd23db8bc2252c0981214be7703939
pivnati <natanmuleta911@gmail.com>
2025-04-15 15:37:34 -0700

    install the ejs in the

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
-app.use((req,res,next)=>{   // register middleware consist of req,res and next()  inside it 
-    console.log('start')
-    res.on('finish', ()=>{
-        console.log('end')
-    })
-    next()
-})
 app.get('/', (req, res) => {
-      console.log('middle') 
     res.send('Hello, this is piv')
 })
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
