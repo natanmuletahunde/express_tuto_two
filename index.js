3f83366cf679027c1dc02ba6b13915b9bc99cba3
pivnati <natanmuleta911@gmail.com>
2025-04-15 15:54:56 -0700

    create the index.ejs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 const app = express()
 const PORT = 3000
+// set the EJS as the view engine 
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('Hello, this is piv')
+   
+    const userName = 'Piv Nati'
+    res.render('index' ,{userName})
 })
 
 // Start the server
