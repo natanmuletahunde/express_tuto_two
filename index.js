3c5189023d19b9abe0c71e23eb994406eec7975f
pivnati <natanmuleta911@gmail.com>
2025-04-16 14:00:05 -0700

    create the crdu opertion

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import { Person } from './models/Person.js'
 const app = express()
 const PORT = 3000
 await connectDB()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('hello express')
 })
 
-app.post('/person' , express.json(), async (req,res)=>{
+app.post('/person' ,async (req,res)=>{
 
-    console.log(req.body)
     const {email,name,age} = req.body
     const newPreson = new Person(
         {
@@ -22,10 +22,28 @@ app.post('/person' , express.json(), async (req,res)=>{
         }
     )
     await newPreson.save()
+    console.log(newPreson)
     res.send('Person Added')
-
 })
+// updating data in the mongodb
+app.put('/person' , express.json() ,async (req,res)=>{
+
+    const {id} = req.body
+
+    const personData = await Person.findById(id)
+    personData.save()
+    console.log(personData) 
+
+    res.send('Person updated')
+})
+
+// deleting Data from the mongodb
+app.delete('/person/:id' , async (req,res)=>{
+    const {id} = req.params
+    await  Person.findByIdAndDelete(id)
+    res.send('user deleted')
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
- 
