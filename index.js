d1d377049ca96c84977a3cbf2a95bdbdb3b49106
pivnati <natanmuleta911@gmail.com>
2025-04-15 21:15:33 -0700

    create the multter.js file in the config folder

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 import express from 'express'
-import multer from 'multer'
-import path from 'path'
-
-// Setup storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({ storage })
-
 const app = express()
 const PORT = 3000
-
-// Middleware to parse URL-encoded and JSON data
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-// Route with upload middleware inside the route (IMPORTANT!)
-app.post('/form', upload.single('image'), (req, res) => {
-  console.log(req.body)   // Text fields
-  console.log(req.file)   // Uploaded file
-  res.send('Form received')
+app.get('/', (req, res) => {
+    res.send('hello express')
 })
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
+ 
