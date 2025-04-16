3c48b87642ccbc0f7d9fcd6ba4d9c51779a71b26
pivnati <natanmuleta911@gmail.com>
2025-04-16 16:39:14 -0700

    install the cookies in the packages

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
 import express from 'express'
 import {connectDB} from './config/db.js'
-import { Person } from './models/Person.js'
-
+import cookieParser from 'cookie-parser'
 const app = express()
 const PORT = 3000
 await connectDB()
-app.use(express.json())
+
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
+    res.cookie('name','express-app')
     res.send('hello express')
 })
-
-app.post('/person' ,async (req,res)=>{
-
-    const {email,name,age} = req.body
-    const newPreson = new Person(
-        {
-            name ,
-            age,
-            email
-        }
-    )
-    await newPreson.save()
-    console.log(newPreson)
-    res.send('Person Added')
-})
-// updating data in the mongodb
-app.put('/person' , express.json() ,async (req,res)=>{
-
-    const {id} = req.body
-
-    const personData = await Person.findById(id)
-    personData.save()
-    console.log(personData) 
-
-    res.send('Person updated')
+app.get('/fetch', (req,res)=>{
+    console.log(req.cookies);
+    res.send('API Called') 
 })
 
-// deleting Data from the mongodb
-app.delete('/person/:id' , async (req,res)=>{
-    const {id} = req.params
-    await  Person.findByIdAndDelete(id)
-    res.send('user deleted')
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
--- models/Person.js
+++ models/Person.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const personSchema =new mongoose.Schema({
-     name:String,
-     age:Number,
-     email:String
-})  
+     name:{type:String,required:true},
+     age:{type:Number,required:true},
+     email:{type:String, required:true,unique:true},
+     userOrder:{type:Object,default:{}}
+},{timestamps:true , minimize:false})  // the minimize:false is used for the put an empty object in the mongodb database 
+  
 
 export const Person = mongoose.model("person" , personSchema)
