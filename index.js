840a36e8bf2f5c5d58cae3cfb2903c294ae7403f
pivnati <natanmuleta911@gmail.com>
2025-04-15 21:43:23 -0700

    connect withe the database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
 const PORT = 3000
+const MONGOOSE_URI ='mongodb+srv://pivnati:piv123@cluster0.yqe9lij.mongodb.net/piv_express_two'
+
+mongoose.connect(MONGOOSE_URI).then(()=>{
+    console.log('db connected')
+})
 app.get('/', (req, res) => {
     res.send('hello express')
 })
